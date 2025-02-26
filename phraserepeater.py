249307b3b74dd20c7efcf959498f4018827a92ed
RohitDen <157422362+RohitDen@users.noreply.github.com>
2025-02-26 13:27:17 -0800

    Create phraserepeater.py

---
+++ phraserepeater.py
@@ -0,0 +1,8 @@
+user_phrase = input("What is the phrase you would like the computer to repeat?")
+
+user_number = int(input("How many times do you want to repeat the phrase?"))
+
+for x in range(user_number):
+   print(user_phrase)
+  
+
